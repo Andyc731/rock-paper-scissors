3e85189c584c551e5a165d09e87e8e6cec5ac69f
Andy <andychc731@gmail.com>
2023-04-07 18:54:52 -0600

    Add rps buttons to play round

--- rps.js
+++ rps.js
@@ -20,23 +20,23 @@ function playRound(playerSelection, computerSelection){
         if (computerSelection === "rock") {
             result = "You Tied! Computer chose rock";
         } else if (computerSelection === "paper") {
-            result = "You Lose! Paper beats rock";
+            result = "You Lose! Computer chose paper";
         } else {
-            result = "You Win! Rock beats scissors";
+            result = "You Win! Computer chose scissors";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            result = "You Win! Paper beats rock";
+            result = "You Win! Computer chose rock";
         } else if (computerSelection === "paper") {
             result = "You Tied! Computer chose paper";
         } else {
-            result = "You Lose! Scissors beats paper";
+            result = "You Lose! Computer chose scissors";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            result = "You Lose! Rock beats scissors";
+            result = "You Lose! Computer chose rock";
         } else if (computerSelection === "paper") {
-            result = "You Win! Scissors beats paper";
+            result = "You Win! Computer chose paper";
         } else {
             result = "You Tied! Computer chose scissors";
         }
@@ -52,18 +52,17 @@ function game(){
     let computerScore = 0;
     let tie = 0;
 
-    for (i = 0; i < 5; i++) {
-        let result = playRound(prompt("Enter rock, paper or scissors").toLowerCase(), getComputerChoice());
-        console.log(result);
-        if (result === "You Win! Scissors beats paper" || result === "You Win! Paper beats rock" || result === "You Win! Rock beats scissors") {
-            playerScore++;
-        } else if (result === "You Lose! Paper beats rock" || result === "You Lose! Scissors beats paper" || result === "You Lose! Rock beats scissors") {
-            computerScore++;
-        } else {
-            tie++;
-        }
-        console.log("Current score is: %d:%d tied %d times", playerScore, computerScore, tie);
+    let result = playRound(prompt("Enter rock, paper or scissors").toLowerCase(), getComputerChoice());
+    console.log(result);
+    if (result === "You Win! Scissors beats paper" || result === "You Win! Paper beats rock" || result === "You Win! Rock beats scissors") {
+        playerScore++;
+    } else if (result === "You Lose! Paper beats rock" || result === "You Lose! Scissors beats paper" || result === "You Lose! Rock beats scissors") {
+        computerScore++;
+    } else {
+        tie++;
     }
+    
+    console.log("Current score is: %d:%d tied %d times", playerScore, computerScore, tie);
     if (playerScore < computerScore) {
         console.log("You lost!");
     } else if (playerScore > computerScore) {
@@ -74,4 +73,30 @@ function game(){
 
 }
 
-console.log(game());
+const bod = document.querySelector('body');
+
+const btnRock = document.createElement('button');
+btnRock.classList.add('rock');
+btnRock.textContent = "Rock";
+
+const btnPaper = document.createElement('button');
+btnPaper.classList.add('paper');
+btnPaper.textContent = "Paper";
+
+const btnScissors = document.createElement('button');
+btnScissors.classList.add('scissors');
+btnScissors.textContent = "Scissors";
+
+bod.appendChild(btnRock);
+bod.appendChild(btnPaper);
+bod.appendChild(btnScissors);
+
+btnRock.addEventListener('click', () => {
+    console.log(playRound("rock", getComputerChoice()));
+});
+btnPaper.addEventListener('click', () => {
+    console.log(playRound("paper", getComputerChoice()));
+});
+btnScissors.addEventListener('click', () => {
+    console.log(playRound("scissors", getComputerChoice()));
+});
